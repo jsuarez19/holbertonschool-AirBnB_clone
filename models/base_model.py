37d5a7fe245c43bafb37edcb077e4b5c56e72c98
Jose Suarez <jolusuvi@gmail.com>
2023-08-01 17:46:54 -0700

    docstring info added

--- models/base_model.py
+++ models/base_model.py
@@ -1,22 +1,32 @@
 #!/usr/bin/python3
+"""
+Writing the BaseModel class
+"""
 
-from uuid import uuid4
+
 from datetime import datetime
+from uuid import uuid4
+
 
 class BaseModel:
-
+    """Class BaseModel"""
     def __init__(self):
+        """Constructor"""
         self.id = str(uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
 
     def __str__(self):
-        return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
-    
+        """__str__ method"""
+        name = type(self).__name__
+        return "[{}] ({}) {}".format(name, self.id, self.__dict__)
+
     def save(self):
+        """Updates current datetime"""
         self.updated_at = datetime.now()
 
     def to_dict(self):
+        """Returns a new dictionary"""
         new_dict = self.__dict__
         new_dict["__class__"] = self.__class__.__name__
         new_dict["created_at"] = str(self.created_at.isoformat())
